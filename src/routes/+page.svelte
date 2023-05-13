500825d9a0a41ce56ce44bc75f991375903375ed
thenewlegend <jiyonpj2501@gmail.com>
2023-05-13 14:55:38 +0530

    Iitital Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    import Card from "./Card.svelte";
+    import Time from "svelte-time";
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Time /><br>
+
+<span>Publishes <Time relative timestamp="2023-05-15" /></span>
+
+<Card />
+
+<br>
